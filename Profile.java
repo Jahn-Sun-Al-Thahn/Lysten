2635cf4264ab8db5a8272b2232a67ae3b5983e95
Jahn-Sun-Al-Thahn <78040818+Jahn-Sun-Al-Thahn@users.noreply.github.com>
2021-10-19 09:24:43 -0500

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -61,62 +61,87 @@ public class Profile extends UserActivity{
 	}
 
 
-	public ArrayList<File> getAudioSaves() {
-		return audioSaves;
+	public File getAudioSaves() {
+		Scanner input = new Scanner(System.in);
+		int ch;
+		System.out.println("Which audio Save would you like to choose?");
+		for(int i = 0; i < audioSaves.size(); i++) {
+			System.out.println("Save #" + i + ": " + audioSaves.get(i));
+		}
+		ch = input.nextInt();
+		input.close();
+		return audioSaves.get(ch);
 	}
 
 
-	public ArrayList<Profile> getBlocked() {
-		return Blocked;
+	public void getBlocked() {
+		for(int i = 0; i < Blocked.size(); i++) {
+			System.out.println(Username + " has blocked the following Users: ");
+			System.out.println(Blocked.get(i));
+		}
+		
 	}
 
 
-	public ArrayList<Profile> getFriendsList() {
-		return FriendsList;
+	public Profile getFriendsList() {
+		Scanner input = new Scanner(System.in);
+		int ch;
+		System.out.println("Which Friend would you like to choose?");
+		for(int i = 0; i < FriendsList.size(); i++) {
+			System.out.println("Save #" + i + ": " + FriendsList.get(i));
+		}
+		ch = input.nextInt();
+		input.close();
+		return FriendsList.get(ch);
 	}
 
 
-	public ArrayList<Server> getFavoriteChats() {
+	public Server getFavoriteChats() {
+		
+		//use the get friends list body and substitute the array list name
+		
 		return favoriteChats;
 	}
 
 
-	public ArrayList<Server> getPrivateMessages() {
+	public Server getPrivateMessages() {
+		//use the get friends list body and substitute the array list name
 		return privateMessages;
 	}
 
 
-	public ArrayList<Server> getFavoriteForums() {
+	public Server getFavoriteForums() {
+		//use the get friends list body and substitute the array list name
 		return favoriteForums;
 	}
 
 	//Complete the body of the List Methods.
-	public void AddToForumList(ArrayList<Server> favoriteForums) {
-	
+	public void AddToForumList(Server favoriteForums) {
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
-	public void AddToPMList(ArrayList<Server> privateMessages) {
-		
+	public void AddToPMList(Server privateMessages) {
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
-	public void AddToChatList(ArrayList<Server> favoriteChats) {
-		
+	public void AddToChatList(Server favoriteChats) {
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
 	public void AddToFriendsList(ArrayList<Profile> friendsList) {
-		
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
 	public void AddToBlockedList(ArrayList<Profile> blocked) {
-		
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
 	public void AddToAudioList(ArrayList<File> audioSaves) {
-		
+		//use the arraylist.add() method to add another server to the arraylist
 	}
 	
 	public void RemoveFromForumList(ArrayList<Server> favoriteForums) {
-		
+		//use the arraylist.remove() method to add another server to the arraylist
 	}
 	
 	public void RemoveFromPMList(ArrayList<Server> privateMessages) {
