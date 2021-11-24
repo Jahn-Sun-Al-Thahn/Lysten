caacaf81447f655db5a64d72462943a7119caf80
Jahn-Sun-Al-Thahn <78040818+Jahn-Sun-Al-Thahn@users.noreply.github.com>
2021-11-24 10:16:00 -0600

    Add files via upload

--- MidiInstrument.java
+++ MidiInstrument.java
@@ -1,81 +1,335 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.AbstractButton;
-import javax.swing.GroupLayout;
-import javax.swing.Icon;
-import javax.swing.InputMap;
-import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
-import javax.swing.JInternalFrame;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JPopupMenu;
-import javax.swing.JToggleButton;
-import javax.swing.JWindow;
+
 import javax.swing.border.*;
-import javax.swing.JTextField;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class MidiInstrument extends JFrame {
+public class MidiInstrument extends JPanel {
 
+	private static final long serialVersionUID = 1L;
 	JTextField inGoing;
 	JTextField outGoing;
-	EtchedBorder border;
 	Color bg;
 	Color fore;
 	Color whiteKeys;
 	Color blackKeys;
 	Font fnt;
-	
+	ArrayList<JButton> Keys;
+	JButton Save;
+	JButton Open;
+	JButton Preset;
+	JMenu SoundMiniMenu;
+	Dimension Max;
+	Dimension Min;
+	final JFileChooser SOP = new JFileChooser();
+	private AudioInputStream AudStream;
+	private ArrayList<Clip> keySounds;
+
 	public MidiInstrument() {
-		//initialize fields
-		bg = new Color(154,90,186,0);
-		fore = new Color(154,0,186,1);
-		whiteKeys = new Color(255,255,255,1);
-		blackKeys = new Color(0,0,0,1);
-		border = new EtchedBorder(1);
+
+		// initialize fields
+		bg = new Color(154, 90, 186, 1);
+		fore = new Color(154, 0, 186, 1);
+		whiteKeys = new Color(255, 255, 255, 1);
+		blackKeys = new Color(0, 0, 0, 1);
 		fnt = new Font(Font.SERIF, 4, 12);
-		Dimension Max = new Dimension(650,300);
-		Dimension Min = new Dimension(649,299);
-		
-		//set how the JFrame looks and how the objects within it will be laid out, and enabled
+		Max = new Dimension(520, 185);
+		Min = new Dimension(Max.width, Max.height);
+
+		// set how the JFrame looks and how the objects within it will be laid out, and
+		// enabled
+		setName("KeyBoard");
 		requestFocus();
 		setEnabled(true);
 		setVisible(true);
-		setBounds(45,45,650,300);
-		super.setMaximumSize(Max);
-		super.setMinimumSize(Min);
+		setBounds(0, 0, Max.width, Max.height);
+		setMaximumSize(Max);
+		setMinimumSize(Min);
 		setFont(fnt);
-		setLayout(new FlowLayout(0));
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		
-		//add() can add components or JObjects to the JFrame
-		//borders must be set on JPanels
-		//ACTIONLISTENERS MUST BE SET AFTER IN THE panel section it belongs with
-		
+		setLayout(new BorderLayout());
+		/*
+		 * Next Lines Not Applicable for JPanel, only JFrame setTitle("Keyboard");
+		 * setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		 */
+
+		// add() can add components or JObjects to the JFrame
+		// borders must be set on JPanels
+		// ACTIONLISTENERS MUST BE SET AFTER IN THE panel section it belongs with
+
 		JPanel OuterFrame = new JPanel();
-		add(OuterFrame);
-		OuterFrame.setBounds(5,5,650,300);
-		OuterFrame.setAlignmentX(LEFT_ALIGNMENT);
-		OuterFrame.setBorder(border);
-		
-		JPanel SP1_1 = new JPanel();
-		OuterFrame.add(SP1_1);
-		SP1_1.setBorder(border);
-		inGoing = new JTextField(40);
+		OuterFrame.setAlignmentX(CENTER_ALIGNMENT);
+		OuterFrame.setAlignmentY(CENTER_ALIGNMENT);
+		OuterFrame.setBorder(BorderFactory.createEtchedBorder());
+		OuterFrame.setLayout(new GridLayout(1, 3));
+		OuterFrame.setBackground(Color.BLACK);
+		OuterFrame.setSize(Max.width / 5, (Max.height / 2));
+		Save = new JButton("Save");
+		Save.addActionListener(new FileChooserListener());
+		Open = new JButton("Open");
+		Open.addActionListener(new FileChooserListener());
+		Preset = new JButton("Preset");
+		Preset.addActionListener(new FileChooserListener());
+
+		// Search Text Area, Chosen Search Result/Current SoundFile
+
+		/*
+		 * Add Midi-Control Sound Files from the Midi Sound Directory thats associated
+		 * with this application. Read each file name, add each file name to the
+		 * Mini-menu. So that in Listener the name that is chosen can be found and
+		 * opened "read through the Clip and sent to the mixer to be manipulated".
+		 */
+		SoundMiniMenu = new JMenu();
+		SoundMiniMenu.setBackground(Color.WHITE);
+		SoundMiniMenu.setForeground(Color.GRAY);
+		SoundMiniMenu.setFont(fnt);
+		SoundMiniMenu.setMnemonic(KeyEvent.VK_A);
+		SoundMiniMenu.setText("Choose Sound");
+		JMenuItem FirstEx = new JMenuItem("Example Item");
+		JMenuItem SecondEx = new JMenuItem("Example 2");
+		SoundMiniMenu.insert(FirstEx, 1);
+		SoundMiniMenu.addSeparator();
+		SoundMiniMenu.insert(SecondEx, 2);
+		SoundMiniMenu.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+
+		inGoing = new JTextField((int) (Max.width / 5));
+		inGoing.setText("Search");
+		inGoing.setFont(fnt);
 		inGoing.setHorizontalAlignment(2);
-		SP1_1.add(inGoing);
+		inGoing.setAlignmentX(CENTER_ALIGNMENT);
+		inGoing.setAlignmentY(CENTER_ALIGNMENT);
 		inGoing.addActionListener(null);
-		
-		JPanel SP1_2 = new JPanel();
-		OuterFrame.add(SP1_2);
-		SP1_2.setBorder(border);
+
+		OuterFrame.add(inGoing);
+		OuterFrame.add(SoundMiniMenu);
+		OuterFrame.add(Save);
+		OuterFrame.add(Open);
+		OuterFrame.add(Preset);
+		add(OuterFrame, BorderLayout.NORTH);
+
+		// Make a open panel to save and open midi-instrument files or presets
+		JPanel Keyboard = new JPanel();
+		Keyboard.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		Keyboard.setBackground(Color.BLACK);
+		Keyboard.setLayout(new GridLayout(1, 12));
+		// Keyboard.setSize(WIDTH, HEIGHT);
+		Keys = new ArrayList<>();
+		for (int i = 0; i < 13; i++) {
+			if (i == 1 || i == 4 || i == 6 || i == 9 || i == 11) {
+				// black keys
+				Keys.add(new JButton());
+				Keyboard.add(Keys.get(i));
+				Keys.get(i).setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				Keys.get(i).setBackground(Color.BLACK);
+				Keys.get(i).addActionListener(new BlackKeyListener());
+
+			} else {
+				// white Keys
+				Keys.add(new JButton());
+				Keyboard.add(Keys.get(i));
+				Keys.get(i).setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				Keys.get(i).setBackground(Color.white);
+				Keys.get(i).addActionListener(new WhiteKeyListener());
+			}
+
+		}
+		add(Keyboard, BorderLayout.CENTER);
+
+		keySounds = new ArrayList<>();
+		File defaultClip = new File(
+				"C:\\Users\\Althon Johnson\\OneDrive\\Documents\\Splice\\Tiesto & KSHMR - Secrets (Drop) Project\\Samples\\splice\\Hi Hat.wav");
+		File secondaryClip = new File("C:\\Users\\Merc Kiv\\Music\\Samples\\Instrument\\Single Instrument\\Bass_01");
+		try {
+			// AudStream = AudioSystem.getAudioInputStream(defaultClip);
+			AudStream = AudioSystem.getAudioInputStream(secondaryClip);
+			System.out.println("Used Secondary");
+
+			keySounds.add(AudioSystem.getClip());
+			keySounds.get(0).open(AudStream);
+			keySounds.get(0).addLineListener(null);
+
+		} catch (IOException IOE) {
+			JDialog noAudioFile = new JDialog();
+			noAudioFile.setTitle("No Audio Stream");
+			IOE.getMessage();
+			IOE.getStackTrace();
+		} catch (UnsupportedAudioFileException UAF) {
+			JDialog noAudioFile = new JDialog();
+			noAudioFile.setTitle("No Audio File");
+			UAF.getMessage();
+			UAF.getStackTrace();
+		} catch (LineUnavailableException LUA) {
+			JDialog noAudioFile = new JDialog();
+			noAudioFile.setTitle("No Audio Line");
+			LUA.getMessage();
+			LUA.getStackTrace();
+		}
+
 	}
 
+	public void setDimensions(int width, int height) {
+		this.Max.width = width;
+		this.Max.height = height;
+		this.Min.width = width;
+		this.Min.height = height;
+	}
+
+	public Clip getKeySoundsElement(int Element) {
+		return keySounds.get(Element);
+	}
+
+	public void setKeySoundsElement(Clip keySounds) {
+		this.keySounds.add(keySounds);
+	}
+
+	public AudioInputStream getAudStream() {
+		return AudStream;
+	}
+
+	public void setAudStream(AudioInputStream audStream) {
+		AudStream = audStream;
+	}
+
+	public class SearchListener implements ActionListener, KeyListener {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	public class FileChooserListener implements ActionListener {
+		public void actionPerformed(ActionEvent AE) {
+			if (AE.getSource() == Save) {
+				SOP.getCurrentDirectory();
+				SOP.showSaveDialog(SOP);
+
+			} else if (AE.getSource() == Open) {
+				SOP.getCurrentDirectory();
+				SOP.showOpenDialog(SOP);
+
+			} else if (AE.getSource() == Preset) {
+				SOP.getCurrentDirectory();
+				SOP.showOpenDialog(SOP);
+
+			}
+		}
+	}
+
+	public class BlackKeyListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			keySounds.get(0).start();
+			keySounds.get(0).drain();
+
+			/*
+			 * Can make condition that checks array for clip, against the key that is
+			 * pressed keyposition == arrayposition.
+			 */
+		}
+
+	}
+
+	public class WhiteKeyListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			keySounds.get(0).start();
+			keySounds.get(0).drain();
+			
+		}
+
+	}
 	
+	public class MenuHoverListener implements MenuListener{
+
+		@Override
+		public void menuSelected(MenuEvent e) {
+			JMenu Replace = e.getSource();
+			
+		}
+
+		@Override
+		public void menuDeselected(MenuEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void menuCanceled(MenuEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+
 	public static void main(String[] args) {
 		new MidiInstrument();
 	}
--- Template.java
+++ Template.java
@@ -8,34 +8,66 @@ import java.awt.GridLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JInternalFrame;
 import javax.swing.JToolBar;
+import javax.swing.ScrollPaneLayout;
+
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import javax.sound.sampled.*;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JTextField;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JMenu;
 import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
 import javax.swing.DropMode;
 import javax.swing.JFormattedTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.border.EtchedBorder;
 import javax.swing.JSeparator;
 
 public class Template {
 
 	private JFrame frame;
+	private JPanel title;
+	private JPanel WestMenu;
+	private JPanel EastMenu;
+	private JLayeredPane CenterPanel;
+	private JPanel CenterSouthPanel;
+	private JPanel navigator;
+	private JPanel MsgPanel;
+	private GridLayout WestSideMenu;
+	private GridLayout EastSideMenu;
+	private MidiInstrument keys;
+	JFormattedTextField formattedTextField;
+	private ArrayList<TextMessage> msgs = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -66,61 +98,318 @@ public class Template {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 14));
-		frame.setBounds(100, 100, 450, 800);
-		frame.setTitle("Main Page");
-		//frame.setBackground();
+		frame.setBounds(20, 20, 535, 800);
+		frame.setTitle("Lysten");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel title = new JPanel();
-		FlowLayout flowLayout_1 = (FlowLayout) title.getLayout();
+
+		title = new JPanel();
+		title.setLayout(new BorderLayout());
 		title.setBackground(new Color(0, 128, 0));
+		title.setLocation(5, 5);
+		title.setBorder(BorderFactory.createEtchedBorder(Color.gray, Color.BLACK));
 		frame.getContentPane().add(title, BorderLayout.NORTH);
+
+		JButton sideBar = new JButton("*");
+		sideBar.setSize(20, 20);
+		sideBar.addActionListener(new ToggleVisibleMenuListeners());
+		title.add(sideBar, BorderLayout.WEST);
 		
-		JLabel lblNewLabel = new JLabel("Current Page");
+		JButton rightSideBar = new JButton("*");
+		rightSideBar.setSize(50,30);
+		rightSideBar.addActionListener(new ToggleVisibleMenuListener());
+		title.add(rightSideBar, BorderLayout.EAST);
+		
+		
+
+		JLabel lblNewLabel = new JLabel("Current Chat");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Sitka Text", Font.BOLD, 14));
+		lblNewLabel.setVerticalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
 		lblNewLabel.setForeground(new Color(245, 245, 245));
 		title.add(lblNewLabel);
-		
-		JPanel navigator = new JPanel();
+
+		CenterSouthPanel = new JPanel();
+		CenterSouthPanel.setLayout(new GridLayout(2, 0));
+		frame.getContentPane().add(CenterSouthPanel, BorderLayout.SOUTH);
+
+		navigator = new JPanel();
 		navigator.setBackground(new Color(0, 128, 0));
-		frame.getContentPane().add(navigator, BorderLayout.SOUTH);
+		navigator.setBorder(BorderFactory.createEtchedBorder());
 		navigator.setLayout(new GridLayout(0, 5, 0, 0));
-		
+		CenterSouthPanel.add(navigator);
+
 		JButton btnNewButton_4 = new JButton("Profile");
 		navigator.add(btnNewButton_4);
-		
 		JButton btnNewButton_3 = new JButton("Chats");
 		navigator.add(btnNewButton_3);
-		
 		JButton btnNewButton_2 = new JButton("Main Feed");
 		navigator.add(btnNewButton_2);
-		
 		JButton btnNewButton = new JButton("Forums");
 		navigator.add(btnNewButton);
-		
 		JButton btnNewButton_1 = new JButton("Search");
 		navigator.add(btnNewButton_1);
+
+		JPanel CPanel = new JPanel();
+		CPanel.setLayout(new BorderLayout());
+		CPanel.setBounds(0, 0, 535,643);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		CenterPanel = new JLayeredPane();
+		CenterPanel.setPreferredSize(new Dimension(CPanel.getWidth(), CPanel.getHeight()));
+		CenterPanel.setBackground(Color.BLACK);
+		CenterPanel.setBorder(BorderFactory.createLineBorder(new Color(0, 128, 0)));
+		CPanel.add(CenterPanel, BorderLayout.CENTER);
+		frame.getContentPane().add(CPanel, BorderLayout.CENTER);
+
+		MsgPanel = new JPanel(new GridLayout(100, 1));
+		MsgPanel.setSize(520, 643);
+		MsgPanel.setBorder(BorderFactory.createLineBorder(Color.green));
+		MsgPanel.setVisible(true);
+		CenterPanel.add(MsgPanel, JLayeredPane.PALETTE_LAYER);
+		
+		/*JScrollPane scrollPane = new JScrollPane(MsgPanel);
+		scrollPane.setLayout(new ScrollPaneLayout());
+		scrollPane.setPreferredSize(new Dimension(520, 643));
+		scrollPane.setOpaque(true);
+		scrollPane.setVisible(true);
 		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setViewportView(MsgPanel);
+		CenterPanel.add(scrollPane, JLayeredPane.PALETTE_LAYER);*/
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(169, 169, 169));
-		scrollPane.setViewportView(panel);
-		panel.setLayout(null);
-		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		formattedTextField.setBounds(10, 673, 330, 22);
-		panel.add(formattedTextField);
+
+		keys = new MidiInstrument();
+		keys.setSize(519, 185);
+		keys.setLocation(1, 460);
+		keys.setOpaque(true);
+		keys.setVisible(false);
+		CenterPanel.add(keys, JLayeredPane.POPUP_LAYER);
+
+		formattedTextField = new JFormattedTextField();
+		formattedTextField.setSize(300, 25);
 		formattedTextField.setHorizontalAlignment(SwingConstants.LEFT);
 		formattedTextField.setColumns(30);
 		formattedTextField.setFont(new Font("Liberation Sans", Font.PLAIN, 12));
+		formattedTextField.addActionListener(new TextListener());
+		formattedTextField.addKeyListener(new TextListener());
 		
-		JButton btnNewButton_5 = new JButton("Send");
-		btnNewButton_5.setBounds(356, 672, 62, 23);
-		panel.add(btnNewButton_5);
+		JButton Send = new JButton("Send");
+		Send.setSize(70, 22);
+		Send.addActionListener(new TextListener());
+
+		JButton InstrumentCall = new JButton("Keys");
+		InstrumentCall.addActionListener(new MidiToggleListener());
+
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new FlowLayout());
+		textPanel.setBorder(BorderFactory.createEtchedBorder());
+		textPanel.setBackground(new Color(0, 128, 0));
+		textPanel.setOpaque(true);
+		textPanel.add(Send);
+		textPanel.add(formattedTextField);
+		textPanel.add(InstrumentCall);
+
+		CenterSouthPanel.add(textPanel);
+		CenterSouthPanel.add(navigator);
+
+		WestMenu = new JPanel();
+		WestSideMenu = new GridLayout(8, 1);
+		WestMenu.setLayout(WestSideMenu);
+		WestMenu.setBorder(BorderFactory.createEtchedBorder());
+		JButton Recommend = new JButton("Recommended");
+		JButton Favorites = new JButton("Favorites");
+		JButton Popular = new JButton("Popular");
+		JButton Add = new JButton("Add");
+
+		Popular.setBounds(new Rectangle(10, 10));
+		Popular.setBackground(Color.green);
+		Popular.setBorder(BorderFactory.createRaisedBevelBorder());
+		Popular.addActionListener(null);
+
+		Recommend.setBounds(new Rectangle(10, 10));
+		Recommend.setBackground(Color.green);
+		Recommend.setBorder(BorderFactory.createRaisedBevelBorder());
+		Recommend.addActionListener(null);
+
+		Favorites.setBounds(new Rectangle(10, 10));
+		Favorites.setBackground(Color.green);
+		Favorites.setBorder(BorderFactory.createRaisedBevelBorder());
+		Favorites.addActionListener(null);
+
+		Add.setBounds(new Rectangle(10, 10));
+		Add.setBackground(Color.green);
+		Add.setBorder(BorderFactory.createRaisedBevelBorder());
+		Add.addActionListener(new AddMenuItemListener());
+
+		WestMenu.add(Add);
+		WestMenu.add(Popular);
+		WestMenu.add(Recommend);
+		WestMenu.add(Favorites);
+		
+
+		frame.getContentPane().add(WestMenu, BorderLayout.WEST);
+		WestMenu.setVisible(false);
+		if (WestMenu.isVisible()) {
+			formattedTextField.setSize(formattedTextField.getWidth() - 90, formattedTextField.getHeight());
+		}
+
+		EastMenu = new JPanel();
+		EastSideMenu = new GridLayout(8, 1);
+		EastMenu.setLayout(EastSideMenu);
+		EastMenu.setBorder(BorderFactory.createEtchedBorder());
+		
+		JButton More = new JButton("Add Chat");
+		
+		More.setBounds(new Rectangle(0, 0, 40, 10));
+		More.setBackground(Color.green);
+		More.setBorder(BorderFactory.createRaisedBevelBorder());
+		More.addActionListener(null);
+		
+		EastMenu.add(More);
+		
+		frame.getContentPane().add(EastMenu, BorderLayout.EAST);
+		EastMenu.setVisible(false);
+		if (EastMenu.isVisible()) {
+			formattedTextField.setSize(formattedTextField.getWidth() - 90, formattedTextField.getHeight());
+		}
+		
+
+
 	}
+	
+	public void clearTextArea() throws ParseException{
+		formattedTextField.commitEdit();
+		formattedTextField.setText("");
+	}
+
+	public class ToggleVisibleMenuListeners implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (WestMenu.isVisible() == true) {
+				WestMenu.setVisible(false);
+			} else if (WestMenu.isVisible() == false) {
+				WestMenu.setVisible(true);
+			}
+		}
+
+	}
+	public class ToggleVisibleMenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (EastMenu.isVisible() == true) {
+				EastMenu.setVisible(false);
+			} else if (EastMenu.isVisible() == false) {
+				EastMenu.setVisible(true);
+			}
+		}
+
+	}
+
+	public class SendListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+
+	}
+
+	public class MidiToggleListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (keys.isVisible() == true) {
+				keys.setVisible(false);
+				keys.getKeySoundsElement(0).close();
+			} else if (keys.isVisible() == false) {
+				keys.setVisible(true);
+				try {
+					if(keys.getKeySoundsElement(0).isOpen() == false) {
+						keys.getKeySoundsElement(0).open(keys.getAudStream());
+					}
+				} catch (LineUnavailableException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	public class AddMenuItemListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton Adding = new JButton("New Item");
+			Adding.setBounds(new Rectangle(15, 15));
+			Adding.setBackground(Color.green);
+			Adding.setBorder(BorderFactory.createRaisedBevelBorder());
+			Adding.setVisible(true);
+			Adding.addActionListener(null);
+			WestMenu.add(Adding);
+			WestMenu.updateUI();
+
+		}
+
+	}
+	
+	public class AddRightMenuItemListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton Adding = new JButton("New ChatRoom");
+			JDialog Room = new JDialog();
+			Room.setTitle("New Chat");
+			Adding.setBounds(new Rectangle(15, 15));
+			Adding.setBackground(Color.green);
+			Adding.setBorder(BorderFactory.createRaisedBevelBorder());
+			Adding.setVisible(true);
+			Adding.addActionListener(null);
+			EastMenu.add(Adding);
+			EastMenu.updateUI();
+
+		}
+
+	}
+	
+	public class TextListener implements ActionListener, KeyListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			TextMessage text = new TextMessage(formattedTextField.getText());
+			//MsgPanel.add(text,JLayeredPane.DRAG_LAYER);
+			//MsgPanel.updateUI();
+			CenterPanel.add(text,JLayeredPane.MODAL_LAYER);
+			CenterPanel.updateUI();
+			TextMessage.clearTextArea(formattedTextField);
+			
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(e.isShiftDown() == true && e.getKeyCode() == KeyEvent.VK_ENTER) {
+				String oldText = formattedTextField.getText();
+				formattedTextField.setText(oldText + "\n");
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		
+		}
+	}
+	
+
+	
 }
